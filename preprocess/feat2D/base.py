105adaa2f2954fda79e3fbfb3993720bb48dd1ce
Sayan Deb Sarkar <sayandsarkar.1997@gmail.com>
2025-01-10 16:36:21 -0800

    Added feature preprocessing + utils

---
+++ preprocess/feat2D/base.py
@@ -0,0 +1,32 @@
+import numpy as np
+import torch
+from omegaconf import DictConfig
+from typing import List
+
+from modules.build import build_module
+
+class Base2DProcessor:
+    """Base 2D feature processor class."""
+    def __init__(self, config_data: DictConfig, config_2D: DictConfig, split: str) -> None:    
+        # get device 
+        if not torch.cuda.is_available(): 
+            raise RuntimeError('No CUDA devices available.')
+        self.device = torch.device("cuda")
+         
+        self.model = build_module("2D", config_2D.feature_extractor.model, 
+                                 ckpt =  config_2D.feature_extractor.ckpt, device = self.device)
+
+    def extractFeatures(self, images: List[torch.tensor], return_only_cls_mean: bool = True) -> np.ndarray:
+        """Extract 2D features from a list of images."""
+        image_input = torch.stack(images).to(self.device) 
+        with torch.no_grad(): 
+            ret = self.model.feature_extractor(image_input) 
+            cls_token = ret[:, 0, :]
+        if return_only_cls_mean:
+            return cls_token.mean(dim=0).cpu().detach().numpy().reshape(1, -1)
+        else:
+            return ret.cpu().detach().numpy()
+    
+    def run(self) -> None:
+        """Execute the complete 2D feature extraction pipeline."""
+        self.compute2DFeatures()
---
+++ preprocess/feat3D/scan3r.py
@@ -0,0 +1,73 @@
+import os.path as osp
+import open3d as o3d
+import numpy as np
+import torch
+from omegaconf import DictConfig
+
+from common import load_utils 
+from utils import scan3r
+
+from preprocess.build import PROCESSOR_REGISTRY
+from preprocess.feat3D.base import Base3DProcessor
+
+@PROCESSOR_REGISTRY.register()
+class Scan3R3DProcessor(Base3DProcessor):
+    """
+    Scan3R3DProcessor processes 3D scan data from the Scan3R dataset.
+    """
+
+    def __init__(self, config_data: DictConfig, config_3D: DictConfig, split: str) -> None:
+        self.data_dir = config_data.base_dir
+        
+        files_dir = osp.join(config_data.base_dir, 'files')
+        
+        self.scan_ids = []
+        self.scan_ids = scan3r.get_scan_ids(files_dir, split)
+        
+        self.out_dir = config_data.process_dir
+        load_utils.ensure_dir(self.out_dir)
+        
+        self.label_filename = config_data.label_filename
+        self.undefined = 0
+        
+        self.objects = load_utils.load_json(osp.join(files_dir, 'objects.json'))['scans']
+    
+    def compute3DFeaturesEachScan(self, scan_id: str) -> None:
+        ply_data = scan3r.load_ply_data(osp.join(self.data_dir, 'scans'), scan_id, self.label_filename)
+        mesh_points = np.stack([ply_data['x'], ply_data['y'], ply_data['z']]).transpose((1, 0))
+        
+        mesh = o3d.io.read_triangle_mesh(osp.join(self.data_dir, 'scans', scan_id, self.label_filename))
+        mesh_colors = np.asarray(mesh.vertex_colors)*255.0
+        mesh_colors = mesh_colors.round()
+        
+        scan_objects = [obj_data for obj_data in self.objects if obj_data['scan'] == scan_id][0]['objects']
+        
+        object_pcl_embeddings, object_cad_embeddings = {}, {}
+        object_id_to_label_id = {}
+        for idx, scan_object in enumerate(scan_objects):
+            instance_id = int(scan_object['id'])
+            global_object_id = int(scan_object['global_id'])
+
+            object_pcl = mesh_points[np.where(ply_data['objectId'] == instance_id)]
+            
+            if object_pcl.shape[0] <= self.config_3D.min_points_per_object: 
+                continue
+            
+            assert instance_id not in object_id_to_label_id
+            object_id_to_label_id[instance_id] = global_object_id
+            
+            if object_pcl.shape[0] >= self.config_3D.min_points_per_object:
+                object_pcl_embeddings[instance_id] = self.normalizeObjectPCLAndExtractFeats(object_pcl)
+
+        data3D = {}    
+        data3D['objects'] = {'pcl_embeddings' : object_pcl_embeddings, 'cad_embeddings': object_cad_embeddings}
+        data3D['scene']   = {'pcl_coords': mesh_points[ply_data['objectId'] != self.undefined], 'pcl_feats': mesh_colors[ply_data['objectId'] != self.undefined], 'scene_label' : None}
+            
+        object_id_to_label_id_map = { 'obj_id_to_label_id_map' : object_id_to_label_id}
+        
+        assert len(list(object_id_to_label_id.keys())) >= len(list(object_pcl_embeddings.keys())), 'PC does not match for {}'.format(scan_id)
+        scene_out_dir = osp.join(self.out_dir, scan_id)
+        load_utils.ensure_dir(scene_out_dir)
+            
+        torch.save(data3D, osp.join(scene_out_dir, 'data3D.pt'))
+        torch.save(object_id_to_label_id_map, osp.join(scene_out_dir, 'object_id_to_label_id_map.pt'))
